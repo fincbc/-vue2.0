24b9210514b05fc3f793935d6b4d5ccda46a01a6
fanlinke <finc44@qq.com>
2017-07-12 23:54:37 +0800

    first commit

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,83 @@
+<template lang="html">
+  <div class="cartcontrol">
+    <transition name="move">
+      <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click.stop.prevent="decreaseCart">
+        <span class="inner "></span>
+      </div>
+    </transition>
+    <div class="cart-count" v-show="food.count>0">
+      {{food.count}}
+    </div>
+    <div class="cart-add icon-add_circle" @click.stop.prevent="addCart">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {
+  props: {
+    food: {
+      type: Object
+    }
+  },
+  methods: {
+    addCart(event) {
+      if (!event._constructed) {
+        return;
+      }
+      if (!this.food.count) {
+        Vue.set(this.food, 'count', 1);
+      } else {
+        this.food.count++;
+      }
+      this.$emit('cartadd', event.target);
+    },
+    decreaseCart() {
+      if (!event._constructed) {
+        return;
+      }
+      if (this.food.count) {
+        this.food.count--;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .cartcontrol
+    font-size: 0
+    .move-enter-active, .move-leave-active
+      transition: all .4s linear
+    .move-enter, .move-leave-active
+      opacity: 0
+      transform: translate(24px, 0) rotate(180deg)
+
+
+
+    .cart-decrease
+      display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0, 160, 220)
+
+    .cart-count
+      display: inline-block
+      vertical-align: top
+      width: 12px
+      padding-top: 6px
+      line-height: 24px
+      text-align: center
+      font-size: 10px
+      color: rgb(147, 153, 159)
+    .cart-add
+      display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0, 160, 220)
+</style>
